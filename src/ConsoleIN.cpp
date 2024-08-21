482f6a8b0521a111c4b81192aeff1a92615f4d9e
KatoMisa <kato@rsdlab.jp>
2024-08-21 14:12:28 +0900

    first commit

---
+++ src/ConsoleIN.cpp
@@ -0,0 +1,183 @@
+﻿// -*- C++ -*-
+// <rtc-template block="description">
+/*!
+ * @file  ConsoleIN.cpp
+ * @brief ModuleDescription
+ *
+ */
+// </rtc-template>
+
+#include "ConsoleIN.h"
+
+// Module specification
+// <rtc-template block="module_spec">
+#if RTM_MAJOR_VERSION >= 2
+static const char* const consolein_spec[] =
+#else
+static const char* consolein_spec[] =
+#endif
+  {
+    "implementation_id", "ConsoleIN",
+    "type_name",         "ConsoleIN",
+    "description",       "ModuleDescription",
+    "version",           "1.0.0",
+    "vendor",            "VenderName",
+    "category",          "Test",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+ConsoleIN::ConsoleIN(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_str_inIn("str_in", m_str_in),
+    m_num_inIn("num_in", m_num_in),
+    m_str_outOut("str_out", m_str_out),
+    m_num_outOut("num_out", m_num_out)
+    // </rtc-template>
+{
+}
+
+/*!
+ * @brief destructor
+ */
+ConsoleIN::~ConsoleIN()
+{
+}
+
+
+
+RTC::ReturnCode_t ConsoleIN::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  addInPort("str_in", m_str_inIn);
+  addInPort("num_in", m_num_inIn);
+  
+  // Set OutPort buffer
+  addOutPort("str_out", m_str_outOut);
+  addOutPort("num_out", m_num_outOut);
+
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  
+  // Set CORBA Service Ports
+  
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // </rtc-template>
+
+  
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t ConsoleIN::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+//RTC::ReturnCode_t ConsoleIN::onStartup(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t ConsoleIN::onShutdown(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+RTC::ReturnCode_t ConsoleIN::onActivated(RTC::UniqueId /*ec_id*/)
+{
+  std::cout << "Activate "<< std::endl;
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t ConsoleIN::onDeactivated(RTC::UniqueId /*ec_id*/)
+{
+  std::cout << "Deactivate "<< std::endl;
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t ConsoleIN::onExecute(RTC::UniqueId /*ec_id*/)
+{
+  int num;
+  std::cout << "Please input number: ";
+  std::cin >> num;
+  m_num_out.data = num;
+  std::cout << "Sending to subscriber: " << m_num_out.data << std::endl;
+  m_num_outOut.write(); 
+
+  std::string str;
+  std::cout << "Please input string: ";
+  std::cin >> str;
+  m_str_out.data = CORBA::string_dup(str.c_str());
+  std::cout << "Sending to subscriber: " << m_str_out.data << std::endl;
+  m_str_outOut.write(); 
+
+  return RTC::RTC_OK;
+}
+
+
+//RTC::ReturnCode_t ConsoleIN::onAborting(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t ConsoleIN::onError(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t ConsoleIN::onReset(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t ConsoleIN::onStateUpdate(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t ConsoleIN::onRateChanged(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+
+extern "C"
+{
+ 
+  void ConsoleINInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(consolein_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<ConsoleIN>,
+                             RTC::Delete<ConsoleIN>);
+  }
+  
+}
---
+++ test/src/ConsoleINTest.cpp
@@ -0,0 +1,171 @@
+﻿// -*- C++ -*-
+// <rtc-template block="description">
+/*!
+ * @file  ConsoleINTest.cpp
+ * @brief ModuleDescription (test code)
+ *
+ */
+// </rtc-template>
+
+#include "ConsoleINTest.h"
+
+// Module specification
+// <rtc-template block="module_spec">
+#if RTM_MAJOR_VERSION >= 2
+static const char* const consolein_spec[] =
+#else
+static const char* consolein_spec[] =
+#endif
+  {
+    "implementation_id", "ConsoleINTest",
+    "type_name",         "ConsoleINTest",
+    "description",       "ModuleDescription",
+    "version",           "1.0.0",
+    "vendor",            "VenderName",
+    "category",          "Test",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+ConsoleINTest::ConsoleINTest(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_str_inOut("str_in", m_str_in),
+    m_num_inOut("num_in", m_num_in),
+    m_str_outIn("str_out", m_str_out),
+    m_num_outIn("num_out", m_num_out)
+
+    // </rtc-template>
+{
+}
+
+/*!
+ * @brief destructor
+ */
+ConsoleINTest::~ConsoleINTest()
+{
+}
+
+
+
+RTC::ReturnCode_t ConsoleINTest::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  addInPort("str_out", m_str_outIn);
+  addInPort("num_out", m_num_outIn);
+  
+  // Set OutPort buffer
+  addOutPort("str_in", m_str_inOut);
+  addOutPort("num_in", m_num_inOut);
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  
+  // Set CORBA Service Ports
+  
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // </rtc-template>
+  
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t ConsoleINTest::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+//RTC::ReturnCode_t ConsoleINTest::onStartup(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t ConsoleINTest::onShutdown(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+RTC::ReturnCode_t ConsoleINTest::onActivated(RTC::UniqueId /*ec_id*/)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t ConsoleINTest::onDeactivated(RTC::UniqueId /*ec_id*/)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t ConsoleINTest::onExecute(RTC::UniqueId /*ec_id*/)
+{
+  return RTC::RTC_OK;
+}
+
+
+//RTC::ReturnCode_t ConsoleINTest::onAborting(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t ConsoleINTest::onError(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t ConsoleINTest::onReset(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t ConsoleINTest::onStateUpdate(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t ConsoleINTest::onRateChanged(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+bool ConsoleINTest::runTest()
+{
+    return true;
+}
+
+
+extern "C"
+{
+ 
+  void ConsoleINTestInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(consolein_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<ConsoleINTest>,
+                             RTC::Delete<ConsoleINTest>);
+  }
+  
+}
